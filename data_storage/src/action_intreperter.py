5d0a1d488691e7d17545798ef9d03a42a111c609
Joel-Baptista <joelbaptista@ua.pt>
2022-07-13 12:41:11 +0100

    #7 This commit adds a config.json that serves as a config file for scripts of the data_storage package. It was also refurmalted the action_intreperter.py to fit the new experiment protocol. The variables that influence the trigger were changed to enhance the user experience. Now, the trigger only cares about the 3 moments (x, y, z) and the force in the z axis on the gripper

--- data_storage/src/action_intreperter.py
+++ data_storage/src/action_intreperter.py
@@ -37,45 +37,19 @@ def add_to_vector(data, vector, first_timestamp):
 
 
 def calc_data_mean(data):
+    # values = np.array([data.wrench_force_torque.force.x,
+    #                    data.wrench_force_torque.force.y, data.wrench_force_torque.force.z])
+    # values = np.array([data.wrench_force_torque.force.x,
+    #                    data.wrench_force_torque.force.y, data.wrench_force_torque.force.z, data.wrench_force_torque.torque.x,
+    #                    data.wrench_force_torque.torque.y, data.wrench_force_torque.torque.z])
     values = np.array([data.wrench_force_torque.force.z/10, data.wrench_force_torque.torque.x,
                        data.wrench_force_torque.torque.y, data.wrench_force_torque.torque.z])
+    # # values = np.array([data.joints_effort[0], data.joints_effort[1], data.joints_effort[2], data.joints_effort[3],
+    #                    data.joints_effort[4], data.joints_effort[5], data.wrench_force_torque.force.x,
+    #                    data.wrench_force_torque.force.y, data.wrench_force_torque.force.z, data.wrench_force_torque.torque.x,
+    #                    data.wrench_force_torque.torque.y, data.wrench_force_torque.torque.z])
 
     return np.mean(values)
-
-
-def save_trainnning_data(data, categ):
-
-    path = "./../data/trainning2"
-
-    files = os.listdir(path)
-
-    vector_categ = np.ones((data.shape[0], 1)) * float(categ)
-
-    data = np.append(data, vector_categ, axis=1)
-
-    for file in files:
-        if file.find("learning_data.") != -1:
-            prev_data_array = np.load(path + "/learning_data.npy")
-            data = np.append(prev_data_array, data, axis=0)
-
-    np.save(path + "/learning_data.npy", data.astype('float32'))
-
-    idx_dic = {}
-    idx_list = []
-    for line in data:
-        key = str(int(line[-1]))
-        if not str(key) in idx_dic:
-            idx_dic[key] = 1
-            idx_list.append(key)
-        else:
-            idx_dic[key] += 1
-
-    idx_list.sort()
-
-    print(f"There are now {data.shape[0]} experiments in total")
-
-    for idx in idx_list:
-        print(f"category {idx}: {idx_dic[idx]} experiments")
 
 
 if __name__ == '__main__':
@@ -87,10 +61,6 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Arguments for trainning script")
     parser.add_argument("-ag", "--activate_gripper", type=int, default=0,
                         help="1 - activates gripper; 0 - doesn't activate gripper (default)")
-    parser.add_argument("-n", "--number_reps", type=int, default=1,
-                        help="Corresponds to the number of times the experiment will be performed (default 1)")
-    parser.add_argument("-c", "--category", type=int, default=-1,
-                        help="Classifies the category (default no classification)")
 
     args = vars(parser.parse_args())
 
@@ -103,7 +73,7 @@ if __name__ == '__main__':
     # -------------------------------INITIATE COMMUNICATION----------------------------------------
     # ---------------------------------------------------------------------------------------------
 
-    rospy.init_node("tranning_data_aquisition", anonymous=True)
+    rospy.init_node("action_intreperter", anonymous=True)
 
     data_for_learning = DataForLearning()
     arm_gripper_comm = ArmGripperComm()
@@ -117,7 +87,7 @@ if __name__ == '__main__':
     # ---------------------------------------------------------------------------------------------
 
     try:
-        arm_gripper_comm.move_arm_to_initial_pose()
+        # arm_gripper_comm.move_arm_to_initial_pose()
 
         if args["activate_gripper"] == 1:
             input("Press ENTER to activate gripper in 3 secs")
@@ -147,9 +117,9 @@ if __name__ == '__main__':
 
     trainning_data_array = np.empty((0, limit * config["n_variables"]))
 
-    for j in range(0, args["number_reps"]):
+    while not rospy.is_shutdown():
 
-        print(f"Waiting for action to initiate experiment {j + 1}...")
+        print(f"Waiting for action to initiate prediction ...")
 
         while not rospy.is_shutdown():
             data_mean = calc_data_mean(data_for_learning)
@@ -184,7 +154,6 @@ if __name__ == '__main__':
 
                 data_mean = calc_data_mean(data_for_learning)
                 variance = data_mean - rest_state_mean
-
                 print(variance)
                 if abs(variance) < config["force_threshold_end"]:
                     treshold_counter += 1
@@ -199,29 +168,11 @@ if __name__ == '__main__':
             print("ctrl+C pressed")
 
         if end_experiment:
-            print(f"Data collection interrupted in experiment {j + 1}")
-            trainning_data_array = trainning_data_array[:-1, :]
-            break
+            print("Not enough for prediction")
+        else:
+            print("Let's predict")
+            vector_norm = normalize_data(vector_data, limit)
 
-        vector_norm = normalize_data(vector_data, limit)
-        trainning_data_array = np.append(trainning_data_array, vector_norm, axis=0)
 
     del data_for_learning, arm_gripper_comm
-
-    # ---------------------------------------------------------------------------------------------
-    # -------------------------------SAVE TRAINNING DATA-------------------------------------------
-    # ---------------------------------------------------------------------------------------------
-
-    if args["category"] == -1:
-        category = input("Input the number that corresponds to the category trainned: ")
-    else:
-        category = args["category"]
-
-    out = input(f"You wish to save the {trainning_data_array.shape[0]} experiment? (s/n)\n")
-
-    if out == "s":
-        print(f"Trainning saved!\nIt was saved {trainning_data_array.shape[0]} experiments")
-        save_trainnning_data(trainning_data_array, category)
-    else:
-        print("Trainning not saved!")
 
