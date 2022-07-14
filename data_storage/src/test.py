a0f8264807f59c984cbe7aa901045fcbd1bb8b50
Joel-Baptista <joelbaptista@ua.pt>
2022-07-14 19:24:28 +0100

    Commiting slight changes

--- data_storage/src/test.py
+++ data_storage/src/test.py
@@ -1,19 +1,39 @@
 #!/usr/bin/env python3
 import numpy as np
+from sklearn.preprocessing import normalize
 
 
-path = "../data/trainning/"
-file = "../data/trainning2/test_array.npy"
+def normalize_data(vector, measurements):
 
-array = np.empty((0, 10))
-for i in range(0, 50):
-    row = np.ones((1, 10)) * i
-    array = np.append(array, row, axis=0)
+    data_array = np.reshape(vector, (measurements, int(len(vector) / measurements)))
+    experiment_array_norm = normalize(data_array, axis=0, norm='max')
 
-# np.save(path + file, array)
+    vector_data_norm = np.reshape(experiment_array_norm, (1, vector.shape[0]))
+    return vector_data_norm
 
-print(array)
 
-np.random.shuffle(array)
+# path = "../data/trainning/"
+# file = "../data/trainning2/test_array.npy"
+#
+# array = np.empty((0, 10))
+# for i in range(0, 50):
+#     row = np.ones((1, 10)) * i
+#     array = np.append(array, row, axis=0)
+#
+# # np.save(path + file, array)
+#
+# print(array)
+#
+# np.random.shuffle(array)
+#
+# print(array)
 
-print(array)
+
+A = np.array([[20, 0, -10],
+             [0, 1, 0],
+             [-3, 1, 4]])
+print("Original matrix:")
+print(A)
+print("-------------------------------\nNormalized matrix (by columns):")
+B = normalize(A, axis=0, norm="max")
+print(B)
