e39fcb767f70e91b6b33c45e8e230889acc2ae91
afonsocastro <afonsocastro@ua.pt>
2022-10-04 14:26:06 +0100

    Creating several metrics: accuracy, precision, recall and F1 score pdf output of those metrics_table.pdf

--- neural_networks/keras/testing_n_times.py
+++ neural_networks/keras/testing_n_times.py
@@ -76,6 +76,7 @@ def group_classification(origin_dict, dest_dict):
 
 
 def metrics_calc(origin, metrics_dest, number):
+
     tp = origin["true_positive"][number]
     fp = origin["false_positive"][number]
     fn = origin["false_negative"][number]
@@ -93,7 +94,6 @@ def metrics_calc(origin, metrics_dest, number):
 
 
 def filling_metrics_table(pull_metrics, push_metrics, shake_metrics, twist_metrics):
-
     data = [
         ["", "Mean Accuracy", "Mean Precision", "Mean Recall", "Mean F1", ],
         ["PULL", str(round(statistics.mean(pull_metrics["accuracy"]), 4)),
