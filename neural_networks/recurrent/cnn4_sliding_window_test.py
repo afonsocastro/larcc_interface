887ee45d381a457770405dd1bec52ad32ccb303f
afonsocastro <afonsocastro@ua.pt>
2023-01-20 15:30:16 +0000

    saving predictions for both cnn4 and rnn with Attention

--- neural_networks/recurrent/cnn4_sliding_window_test.py
+++ neural_networks/recurrent/cnn4_sliding_window_test.py
@@ -1,22 +1,9 @@
 #!/usr/bin/env python3
 import keras.models
-import matplotlib.pyplot as plt
-from keras import Input, Model
-import tensorflow as tf
-from keras.callbacks import EarlyStopping
-# from keras.utils import to_categorical  # one-hot encode target column
-from tensorflow.keras.utils import to_categorical  # one-hot encode target column
-from keras.layers import Dense, LSTM, Layer
-from keras.utils.vis_utils import plot_model
-from sklearn.metrics import ConfusionMatrixDisplay
-from sklearn.metrics import confusion_matrix
-from progressbar import progressbar
 from config.definitions import ROOT_DIR
 from larcc_classes.data_storage.SortedDataForLearning import SortedDataForLearning
 import numpy as np
-from neural_networks.utils import plot_confusion_matrix_percentage
-from keras.layers import Lambda
-from keras import backend as K
+
 from numpy import save, delete
 
 
@@ -43,15 +30,16 @@ if __name__ == '__main__':
     n_test = test_data.shape[0]
 
     cnn4_model = keras.models.load_model(ROOT_DIR + "/neural_networks/convolutional/cnn4_model_02_time_window")
+    Bahdanau_Attention_model = keras.models.load_model("model_Bahdanau_Attention")
 
     x_test = np.reshape(test_data[:, :-1], (int(n_test / 2), time_steps, 13))
     y_test = test_data[:, -1]
     x_test_rnn = x_test[:, :, 1:]
     x_test_cnn = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], x_test.shape[2], 1))
 
+    # CONVOLUTIONAL TESTING
     pred_cnn = []
-    # for i in range(0, len(x_test)-1):
-    for i in range(0, 2):
+    for i in range(0, len(x_test)-1):
         sample_pred = []
         for sw in range(0, 81):
             prediction = cnn4_model.predict(x=x_test_cnn[0:1, sw:sw+20, :, :], verbose=0)
@@ -63,5 +51,16 @@ if __name__ == '__main__':
     pred_cnn = np.reshape(pred_cnn, (pred_cnn.shape[0], pred_cnn.shape[1], pred_cnn.shape[3]))
 
     save('cnn4_model_pred.npy', pred_cnn)
+
+    # RECURRENT TESTING
+    pred = Bahdanau_Attention_model.predict(x_test_rnn[0:260].reshape(260, time_steps, params), batch_size=2)
+    save('Bahdanau_Attention_model_pred.npy', pred)
+
+    # TRUE RESULTS
+    y_test_final = []
+    for line in range(0, y_test.shape[0], 2):
+        r = [int(y_test[line]), int(y_test[line + 1])]
+        y_test_final.append(r)
+    save('true_results_cnn4_vs_rnnAttention.npy', y_test_final[0:260])
 
 
