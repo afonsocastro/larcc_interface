837fd5711556254e713692106f63554af9b6edee
afonsocastro <afonsocastro@ua.pt>
2023-02-23 17:11:51 +0000

    trying new RNN model

--- neural_networks/recurrent/seq2seq_fulltime_test.py
+++ neural_networks/recurrent/seq2seq_fulltime_test.py
@@ -1,18 +1,10 @@
 #!/usr/bin/env python3
 import keras.models
-import matplotlib.pyplot as plt
 from keras import Input, Model
 import tensorflow as tf
-from keras.callbacks import EarlyStopping
-# from keras.utils import to_categorical  # one-hot encode target column
-from tensorflow.keras.utils import to_categorical  # one-hot encode target column
 from keras.layers import Dense, LSTM, Layer
-from keras.utils.vis_utils import plot_model
-from sklearn.metrics import ConfusionMatrixDisplay
-from sklearn.metrics import confusion_matrix
-from progressbar import progressbar
 from config.definitions import ROOT_DIR
-from larcc_classes.data_storage.SortedDataForLearning import SortedDataForLearning
+from data_storage.full_timewindow.src.ProcessData import ProcessData
 import numpy as np
 from neural_networks.utils import plot_confusion_matrix_percentage
 from keras.layers import Lambda
@@ -145,7 +137,7 @@ if __name__ == '__main__':
     validation_split = 0.3
     batch_size = 2
     # time_steps = 50
-    time_steps = 100
+    time_steps = 6000
     neurons = 16
     params = 12
     labels = 4
@@ -156,43 +148,45 @@ if __name__ == '__main__':
     # delete('pred_model_Bahdanau_Attention.npy')
     # delete('true_model_Bahdanau_Attention.npy')
 
-    sorted_data_for_learning = SortedDataForLearning(
-        path=ROOT_DIR + "/data_storage/data/raw_learning_data/user_splitted_data/")
+    processed_data = ProcessData(path=ROOT_DIR + "/data_storage/full_timewindow/data/",
+                                 data_file="raw_learning_data.npy", div=0.7)
 
-    training_data = sorted_data_for_learning.training_data
-    test_data = sorted_data_for_learning.test_data
+    test_data = processed_data.test_data_60s
 
-    n_train = len(training_data) * (1 - validation_split)
-    n_val = len(training_data) * validation_split
     n_test = test_data.shape[0]
 
-    model_Bahdanau_Attention = keras.models.load_model("model_Bahdanau_Attention")
+    model_Bahdanau_Attention2 = keras.models.load_model("model_Bahdanau_Attention2")
 
-    x_test = np.reshape(test_data[:, :-1], (int(n_test / 2), time_steps, 13))
-    y_test = test_data[:, -1]
-    x_test = x_test[:, :, 1:]
-
-    y_test_final = []
-    for line in range(0, y_test.shape[0], 2):
-        r = [int(y_test[line]), int(y_test[line + 1])]
-        y_test_final.append(r)
+    x_test = test_data[:, : , 1:-1]
+    y_test = test_data[:, :,  -1]
 
     print("x_test.shape")
     print(x_test.shape)
+    print("y_test.shape")
+    print(y_test.shape)
 
-    nv = x_test[0:260].reshape(260, time_steps, params)
-    print("nv.shape")
+
+    # y_test_final = []
+    # for sample in range(0, y_test.shape[0]):
+    #     for ts in range(0, y_test.shape[1]):
+    #         if y_test[sample][ts]
+    #         r = [int(y_test[line]), int(y_test[line + 1])]
+    #     y_test_final.append(r)
+
+    nv = x_test[0:18].reshape(18, time_steps, params)
     print(nv.shape)
-    
-    pred = model_Bahdanau_Attention.predict(x_test[0:260].reshape(260, time_steps, params), batch_size=2)
+    exit(0)
+
+    pred = model_Bahdanau_Attention2.predict(x_test[0:18].reshape(18, time_steps, params), batch_size=2)
 
     print("type(pred)")
     print(type(pred))
     print("pred.shape")
     print(pred.shape)
-    #
-    # save('pred_model_Bahdanau_Attention.npy', pred)
-    # save('true_model_Bahdanau_Attention.npy', y_test_final[0:260])
+    # exit(0)
+
+    save('pred_model_Bahdanau_Attention2.npy', pred)
+    save('true_model_Bahdanau_Attention2.npy', y_test[0:18])
 
     # print('input: ', x_test[0:2].shape)
     # print('expected: ', y_test_final)
