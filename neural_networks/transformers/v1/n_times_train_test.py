a7b00e9fefd03b375b27b5373102f1ce5ad50079
Afonso Castro <afonsocastro@ua.pt>
2025-02-13 17:20:18 +0000

    100 simulations v1_1 ok. v1_2 doubt

--- neural_networks/transformers/v1/n_times_train_test.py
+++ neural_networks/transformers/v1/n_times_train_test.py
@@ -23,76 +23,83 @@ from larcc_interface.neural_networks.transformers.v1.create_models import (creat
 from rich.progress import track
 
 if __name__ == '__main__':
-    # tf.config.set_visible_devices([], 'GPU')
     n_times = 100
     validation_split = 0.3
     labels = ['PULL', 'PUSH', 'SHAKE', 'TWIST']
-    final_dict = []
+    models = ["model_v1_1", "model_v1_2", "model_v1_3", "model_v1_4", "model_v1_5"]
+    for transformer_model_olarilole in models:
+        training_test_list = []
+        for n in progressbar(range(n_times), redirect_stdout=True):
+        # for n in track(range(n_times), total=n_times):
+            training_data = np.load(ROOT_DIR + "/data_storage/data1/global_normalized_train_data_20ms.npy")
+            x_train = np.reshape(training_data[:, :-1], (training_data.shape[0], 20, 13))
+            y_train = to_categorical(training_data[:, -1])
 
-    training_test_list = []
-    for n in progressbar(range(n_times), redirect_stdout=True):
-    # for n in track(range(n_times), total=n_times):
-        training_data = np.load(ROOT_DIR + "/data_storage/data1/global_normalized_train_data_20ms.npy")
-        x_train = np.reshape(training_data[:, :-1], (training_data.shape[0], 20, 13))
-        y_train = to_categorical(training_data[:, -1])
+            x_train = x_train[:, :, 1:]
+            if transformer_model_olarilole == "model_v1_1":
+                model, model_name = create_transformer_v1_1()
+            elif transformer_model_olarilole == "model_v1_2":
+                model, model_name = create_transformer_v1_2()
+            elif transformer_model_olarilole == "model_v1_3":
+                model, model_name = create_transformer_v1_3()
+            elif transformer_model_olarilole == "model_v1_4":
+                model, model_name = create_transformer_v1_4()
+            elif transformer_model_olarilole == "model_v1_5":
+                model, model_name = create_transformer_v1_5()
 
-        x_train = x_train[:, :, 1:]
+            model.compile(optimizer=Adam(learning_rate=1e-4), loss='categorical_crossentropy', metrics=['accuracy'])
+            model.summary()
 
-        model, model_name = create_transformer_v1_0()
+            fit_history = model.fit(x_train, y_train, shuffle=True, validation_split=validation_split, epochs=500,
+                                    batch_size=32)
 
-        model.compile(optimizer=Adam(learning_rate=1e-4), loss='categorical_crossentropy', metrics=['accuracy'])
-        model.summary()
+            print("\n")
+            print("Using %d samples for training and %d for validation" % (
+            len(training_data) * (1 - validation_split), len(training_data) * validation_split))
 
-        fit_history = model.fit(x_train, y_train, shuffle=True, validation_split=validation_split, epochs=500,
-                                batch_size=32)
+            print("\n")
+            print("-------------------------------------------------------------------------------------------------")
+            print("TESTING: %d simulation" % n)
+            print("-------------------------------------------------------------------------------------------------")
+            print("\n")
 
-        print("\n")
-        print("Using %d samples for training and %d for validation" % (
-        len(training_data) * (1 - validation_split), len(training_data) * validation_split))
+            test_data_1 = np.load(ROOT_DIR + "/data_storage/data1/global_normalized_test_data_20ms.npy")
 
-        print("\n")
-        print("-------------------------------------------------------------------------------------------------")
-        print("TESTING: %d simulation" % n)
-        print("-------------------------------------------------------------------------------------------------")
-        print("\n")
+            x_test = test_data_1[:, :-1]
+            y_test = test_data_1[:, -1]
+            x_test = np.reshape(x_test, (test_data_1.shape[0], 20, 13, 1))
 
-        test_data_1 = np.load(ROOT_DIR + "/data_storage/data1/global_normalized_test_data_20ms.npy")
+            x_test = x_test[:, :, 1:, :]
 
-        x_test = test_data_1[:, :-1]
-        y_test = test_data_1[:, -1]
-        x_test = np.reshape(x_test, (test_data_1.shape[0], 20, 13, 1))
+            predictions_list = []
 
-        x_test = x_test[:, :, 1:, :]
+            pull = {"true_positive": 0, "false_positive": 0, "false_negative": 0, "true_negative": 0}
+            push = {"true_positive": 0, "false_positive": 0, "false_negative": 0, "true_negative": 0}
+            shake = {"true_positive": 0, "false_positive": 0, "false_negative": 0, "true_negative": 0}
+            twist = {"true_positive": 0, "false_positive": 0, "false_negative": 0, "true_negative": 0}
 
-        predictions_list = []
+            for i in range(0, len(test_data_1)):
+                prediction = model.predict(x=x_test[i:i + 1, :, :, :], verbose=0)
+                # Reverse to_categorical from keras utils
+                decoded_prediction = np.argmax(prediction, axis=1, out=None)
+                true = y_test[i]
 
-        pull = {"true_positive": 0, "false_positive": 0, "false_negative": 0, "true_negative": 0}
-        push = {"true_positive": 0, "false_positive": 0, "false_negative": 0, "true_negative": 0}
-        shake = {"true_positive": 0, "false_positive": 0, "false_negative": 0, "true_negative": 0}
-        twist = {"true_positive": 0, "false_positive": 0, "false_negative": 0, "true_negative": 0}
+                prediction_classification_absolute(cla=0, true_out=true, dec_pred=decoded_prediction, dictionary=pull)
+                prediction_classification_absolute(cla=1, true_out=true, dec_pred=decoded_prediction, dictionary=push)
+                prediction_classification_absolute(cla=2, true_out=true, dec_pred=decoded_prediction, dictionary=shake)
+                prediction_classification_absolute(cla=3, true_out=true, dec_pred=decoded_prediction, dictionary=twist)
 
-        for i in range(0, len(test_data_1)):
-            prediction = model.predict(x=x_test[i:i + 1, :, :, :], verbose=0)
-            # Reverse to_categorical from keras utils
-            decoded_prediction = np.argmax(prediction, axis=1, out=None)
-            true = y_test[i]
+                predictions_list.append(decoded_prediction)
 
-            prediction_classification_absolute(cla=0, true_out=true, dec_pred=decoded_prediction, dictionary=pull)
-            prediction_classification_absolute(cla=1, true_out=true, dec_pred=decoded_prediction, dictionary=push)
-            prediction_classification_absolute(cla=2, true_out=true, dec_pred=decoded_prediction, dictionary=shake)
-            prediction_classification_absolute(cla=3, true_out=true, dec_pred=decoded_prediction, dictionary=twist)
+            predicted_values = np.asarray(predictions_list)
 
-            predictions_list.append(decoded_prediction)
+            cm = confusion_matrix(y_true=y_test, y_pred=predicted_values)
+            cm_true = cm / cm.astype(float).sum(axis=1)
+            cm_true_percentage = cm_true * 100
 
-        predicted_values = np.asarray(predictions_list)
+            test_dict = {"cm_true": cm_true, "cm": cm, "pull": pull, "push": push, "shake": shake, "twist": twist}
+            training_test_dict = {"training": fit_history.history, "test": test_dict}
+            training_test_list.append(training_test_dict)
 
-        cm = confusion_matrix(y_true=y_test, y_pred=predicted_values)
-        cm_true = cm / cm.astype(float).sum(axis=1)
-        cm_true_percentage = cm_true * 100
-
-        test_dict = {"cm_true": cm_true, "cm": cm, "pull": pull, "push": push, "shake": shake, "twist": twist}
-        training_test_dict = {"training": fit_history.history, "test": test_dict}
-        training_test_list.append(training_test_dict)
-
-    with open(str(n_times) + "_times_train_test_" + model_name + ".json", "w") as write_file:
-        json.dump(training_test_list, write_file, cls=NumpyArrayEncoder)
+        with open(str(n_times) + "_times_train_test_" + model_name + ".json", "w") as write_file:
+            json.dump(training_test_list, write_file, cls=NumpyArrayEncoder)
