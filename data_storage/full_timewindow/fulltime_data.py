5c831bf969d45b697d0b7d38b9ecf1da9c2ac03b
Afonso Castro <afonsocastro@ua.pt>
2026-03-13 17:55:35 +0000

    halfway done for fulltime dataset3 new acquisiton

--- data_storage/full_timewindow/fulltime_data.py
+++ data_storage/full_timewindow/fulltime_data.py
@@ -1,49 +1,47 @@
 #!/usr/bin/env python3
-import argparse
-from datetime import datetime
-import json
+
 import os
 import time
-from std_msgs.msg import String
-
-from larcc_classes.data_storage.DataForLearning import DataForLearning
 import numpy as np
 import rospy
+from std_msgs.msg import String
+from larcc_classes.data_storage.DataForLearning import DataForLearning
 
 
 class FulltimeData:
+
     def __init__(self):
-        self.vector_data = None
+
         self.data_for_learning = DataForLearning()
+
+        self.actions = ["PUXAR", "EMPURRAR", "ABANAR", "TORCER"]
+
+        self.vector_data = []
+        self.current_class = None
+        self.recording = False
+        self.first_time_stamp = None
 
         time.sleep(0.2)
 
-        self.actions = ["PUXAR", "EMPURRAR", "ABANAR", "TORCER"]
-        # self.actions = ["PULL", "PUSH", "SHAKE", "TWIST"]
+        # ---------------- CALIBRATION ----------------
 
-        self.first_time_stamp = None
-        self.vector_data = np.empty((0, 14))
-
-        dic_offset_calibration = {"fx": [],
-                                  "fy": [],
-                                  "fz": [],
-                                  "mx": [],
-                                  "my": [],
-                                  "mz": [],
-                                  "j0": [],
-                                  "j1": [],
-                                  "j2": [],
-                                  "j3": [],
-                                  "j4": [],
-                                  "j5": []}
+        dic_offset_calibration = {
+            "fx": [], "fy": [], "fz": [],
+            "mx": [], "my": [], "mz": [],
+            "j0": [], "j1": [], "j2": [],
+            "j3": [], "j4": [], "j5": []
+        }
 
         print("CALIBRATING...")
+
         rate = rospy.Rate(100)
-        for i in range(0, 50):
-            # print(self.data_for_learning)
+
+        for _ in range(50):
+
             dic_offset_calibration["fx"].append(self.data_for_learning.wrench_force_torque.force.x)
             dic_offset_calibration["fy"].append(self.data_for_learning.wrench_force_torque.force.y)
             dic_offset_calibration["fz"].append(self.data_for_learning.wrench_force_torque.force.z)
+
             dic_offset_calibration["mx"].append(self.data_for_learning.wrench_force_torque.torque.x)
             dic_offset_calibration["my"].append(self.data_for_learning.wrench_force_torque.torque.y)
             dic_offset_calibration["mz"].append(self.data_for_learning.wrench_force_torque.torque.z)
@@ -54,35 +52,65 @@ class FulltimeData:
             dic_offset_calibration["j3"].append(self.data_for_learning.joints_effort[3])
             dic_offset_calibration["j4"].append(self.data_for_learning.joints_effort[4])
             dic_offset_calibration["j5"].append(self.data_for_learning.joints_effort[5])
+
             rate.sleep()
 
         self.dic_offset = self.offset_calculation(dic_offset_calibration)
+
         print("CALIBRATED")
+
         rospy.Subscriber("ground_truth", String, self.callback_interface)
+
+        # ---------------- DATA ACQUISITION LOOP ----------------
+
+        self.acquire_loop()
+
+    # ------------------------------------------------------------
 
     def callback_interface(self, msg):
 
-        classification = msg.data
+        classification = msg.data.upper()
 
-        classification_int = None
-        if classification.upper() == self.actions[0]:
-            classification_int = 0
-        elif classification.upper() == self.actions[1]:
-            classification_int = 1
-        elif classification.upper() == self.actions[2]:
-            classification_int = 2
-        elif classification.upper() == self.actions[3]:
-            classification_int = 3
-        elif classification.upper() == 'END':
-            save_experiment = input("Save data? (s/n)")
+        if classification == "START":
+            print("START RECORDING")
+            self.vector_data = []
+            self.first_time_stamp = None
+            self.recording = True
+            return
+
+        if classification == "END":
+
+            print("END RECORDING")
+
+            self.recording = False
+
+            save_experiment = input("Save data? (s/n) ")
+
             if "s" in save_experiment.lower():
-                self.save_trainnning_data(self.vector_data)
+                self.save_trainnning_data(np.array(self.vector_data))
                 print("Data saved")
             else:
                 print("Data NOT saved")
 
-        if classification_int is not None:
-            self.add_to_vector(classification_int)
+            return
+
+        if classification in self.actions:
+            self.current_class = self.actions.index(classification)
+
+    # ------------------------------------------------------------
+
+    def acquire_loop(self):
+
+        rate = rospy.Rate(100)
+
+        while not rospy.is_shutdown():
+
+            if self.recording and self.current_class is not None:
+                self.add_to_vector(self.current_class)
+
+            rate.sleep()
+
+    # ------------------------------------------------------------
 
     def add_to_vector(self, class_int):
 
@@ -94,21 +122,26 @@ class FulltimeData:
         else:
             timestamp = data.timestamp() - self.first_time_stamp
 
-        new_data = np.array([timestamp, data.joints_effort[0] - self.dic_offset["j0"],
-                             data.joints_effort[1] - self.dic_offset["j1"],
-                             data.joints_effort[2] - self.dic_offset["j2"],
-                             data.joints_effort[3] - self.dic_offset["j3"],
-                             data.joints_effort[4] - self.dic_offset["j4"],
-                             data.joints_effort[5] - self.dic_offset["j5"],
-                             data.wrench_force_torque.force.x - self.dic_offset["fx"],
-                             data.wrench_force_torque.force.y - self.dic_offset["fy"],
-                             data.wrench_force_torque.force.z - self.dic_offset["fz"],
-                             data.wrench_force_torque.torque.x - self.dic_offset["mx"],
-                             data.wrench_force_torque.torque.y - self.dic_offset["my"],
-                             data.wrench_force_torque.torque.z - self.dic_offset["mz"],
-                             class_int])
+        new_data = [
+            timestamp,
+            data.joints_effort[0] - self.dic_offset["j0"],
+            data.joints_effort[1] - self.dic_offset["j1"],
+            data.joints_effort[2] - self.dic_offset["j2"],
+            data.joints_effort[3] - self.dic_offset["j3"],
+            data.joints_effort[4] - self.dic_offset["j4"],
+            data.joints_effort[5] - self.dic_offset["j5"],
+            data.wrench_force_torque.force.x - self.dic_offset["fx"],
+            data.wrench_force_torque.force.y - self.dic_offset["fy"],
+            data.wrench_force_torque.force.z - self.dic_offset["fz"],
+            data.wrench_force_torque.torque.x - self.dic_offset["mx"],
+            data.wrench_force_torque.torque.y - self.dic_offset["my"],
+            data.wrench_force_torque.torque.z - self.dic_offset["mz"],
+            class_int
+        ]
 
-        self.vector_data = np.append(self.vector_data, [new_data], axis=0)
+        self.vector_data.append(new_data)
+
+    # ------------------------------------------------------------
 
     def offset_calculation(self, dic):
 
@@ -119,34 +152,30 @@ class FulltimeData:
 
         return dic_offset_mean
 
+    # ------------------------------------------------------------
+
     def save_trainnning_data(self, data):
 
-        path = "data"
+        filepath = "data/raw_learning_data.npy"
 
-        files = os.listdir(path)
+        if os.path.exists(filepath):
 
-        file_exists = False
+            prev_data = np.load(filepath, allow_pickle=True)
+            new_data = np.concatenate((prev_data, [data]), axis=0)
 
-        for file in files:
-            if file.find("raw_learning_data.") != -1:
-                print("FILE FOUND")
-                prev_data_array = np.load(path + "/raw_learning_data.npy")
-                data = np.append(prev_data_array, [data], axis=0)
-                file_exists = True
-
-        if file_exists is False:
-            print("FILE NOT FOUND")
-            # data_aux = np.zeros((data.shape[0], data.shape[1], 0))
-            # print(data_aux)
-            # data_aux[:, :, 0] = data
-            np.save(path + "/raw_learning_data.npy", [data])
         else:
-            print("SAVING FOUND FILE")
-            np.save(path + "/raw_learning_data.npy", data)
 
+            new_data = np.array([data])
+
+        np.save(filepath, new_data)
+
+
+# ------------------------------------------------------------
 
 if __name__ == '__main__':
+
     rospy.init_node("training_data_aquisition", anonymous=True)
-    ftd = FulltimeData()
+
+    FulltimeData()
 
     rospy.spin()
--- data_storage/full_timewindow/interface.py
+++ data_storage/full_timewindow/interface.py
@@ -18,30 +18,53 @@ if __name__ == '__main__':
     root = tk.Tk()
     root.title("Guide")
     root.geometry("1920x1080")
-
-    # primitives = ["PULL", "PUSH", "SHAKE", "TWIST"]
-    primitives = ["PUXAR", "EMPURRAR", "ABANAR", "TORCER"]
-
-    xtime = 60
-
     mixer.init()
     sound = mixer.Sound(ROOT_DIR + "/data_storage/full_timewindow/beep-07a.wav")
-    # sound = mixer.Sound("beep-06.wav")
 
-    while True:
-        times = np.random.choice(range(5, 12), size=random.randint(5, 12), replace=True)
-        # times = np.random.choice(range(5, 15), size=random.randint(4, 12), replace=True)
-        if sum(times) == xtime:
-            break
+    primitives = ["PUXAR", "EMPURRAR", "ABANAR", "TORCER"]
+    xtime = 15
+    min_t = 2
+    max_t = 6
 
-    experiment = []
     Stamp = namedtuple("Stamp", "time primitive")
 
-    for t in times:
-        experiment.append(Stamp(t, random.choice(primitives)))
+    def generate_times(total, min_t, max_t):
+        # número possível de ações
+        min_n = (total + max_t - 1) // max_t
+        max_n = total // min_t
+        while True:
+            n = random.randint(min_n, max_n)
+            # começa com tempo mínimo
+            times = [min_t] * n
+            remaining = total - n * min_t
+            # distribui o tempo restante
+            while remaining > 0:
+                i = random.randint(0, n - 1)
+                if times[i] < max_t:
+                    times[i] += 1
+                    remaining -= 1
+            if sum(times) == total:
+                random.shuffle(times)
+                return times
+
+    def generate_primitives(n, primitives):
+        seq = []
+        last = None
+        for _ in range(n):
+            choices = [p for p in primitives if p != last]
+            p = random.choice(choices)
+            seq.append(p)
+            last = p
+        return seq
+
+    times = generate_times(xtime, min_t, max_t)
+    prims = generate_primitives(len(times), primitives)
+
+    experiment = [Stamp(t, p) for t, p in zip(times, prims)]
 
     print("experiment")
     print(experiment)
+    # exit(0)
 
     label = tk.Label(root, text="Please, perform a continuous: ", font=("Arial", 25), pady=30)
     label.pack()
@@ -84,6 +107,7 @@ if __name__ == '__main__':
         while True:
             if temp <= 0:
                 if xtime <= 0:
+                    pub.publish("END")
                     messagebox.showinfo("Experiment Ended", "We got everything we need :)\nThank you!")
                     root.destroy()
                 break
@@ -93,7 +117,7 @@ if __name__ == '__main__':
             xtime -= 0.01
             message = str(stamp.primitive)
             if temp >= 0:
-                pub.publish(message)
+                    pub.publish(message)
 
             str_temp.set(str(timedelta(seconds=int(temp))))
             str_time.set(str(timedelta(seconds=int(xtime))))
@@ -111,5 +135,5 @@ if __name__ == '__main__':
 
             root.update()
 
-    pub.publish("END")
+    # pub.publish("END")
     root.mainloop()
